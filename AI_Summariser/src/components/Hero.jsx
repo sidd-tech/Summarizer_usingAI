caca84e683be43bc57369e35811275f0658d10e9
sidd-tech <57222634+sidd-tech@users.noreply.github.com>
2023-05-16 17:40:18 +0530

    upload project

---
+++ AI_Summariser/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import { logo } from '../assets';
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
+        <button type="button" onClick={() => window.open('https://github.com/sidd-tech')} className="black_btn">
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarise Articles with <br className='max-md:hidden'/>
+        <span className='orange_gradient'> OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summize, and open-source article summariser
+        that transforms lengthy articles into clear and concise summaries.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
